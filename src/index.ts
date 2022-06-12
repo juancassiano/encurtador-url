8d0466cbd0e456a63abe3619c1adf4c6e1fb9cb7
Juan Cassiano <juancassiano@hotmail.com>
2022-06-12 11:46:45 -0300

    Create shorten method

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,11 @@
-import express from "express";
+import express, { Request, Response } from "express";
+import { URLController } from "./controllers/URLController";
 
 const api = express();
+api.use(express.json());
+const urlController = new URLController();
+
+api.post("/shorten", urlController.shorten);
 
 api.listen("5000", () => {
   console.log("Server is running");
