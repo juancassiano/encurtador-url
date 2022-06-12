08caeff6696745110b38e879e37c800f49df9012
Juan Cassiano <juancassiano@hotmail.com>
2022-06-12 13:46:48 -0300

    create Model URL in mongoose

--- src/controllers/URLController.ts
+++ src/controllers/URLController.ts
@@ -1,3 +1,4 @@
+import { URLModel } from "../database/model/Url";
 import { Request, response, Response } from "express";
 import shortId from "shortid";
 import { config } from "../config/Constants";
@@ -5,14 +6,20 @@ import { config } from "../config/Constants";
 export class URLController {
   public async shorten(req: Request, res: Response): Promise<void> {
     const { originURL } = req.body;
+
+    const url = await URLModel.findOne({ originURL });
+
+    if (url) {
+      res.json(url);
+      return;
+    }
+
     const hash = shortId.generate();
     const shortURL = `${config.API_URL}/${hash}`;
 
-    res.json({
-      originURL,
-      hash,
-      shortURL,
-    });
+    const newUrl = await URLModel.create({ hash, shortURL, originURL });
+
+    res.json(newUrl);
   }
 
   public async redirect(req: Request, res: Response): Promise<void> {
